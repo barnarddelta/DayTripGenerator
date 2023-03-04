e89b8d3628b0ea1674128ecee40a59fb133aecbd
Michael Barnard <bernardisntmyname@gmail.com>
2023-03-03 21:28:26 -0800

    Almost complete DaytripGenerator

--- DaytripGenerator.py
+++ DaytripGenerator.py
@@ -1,4 +1,4 @@
-
+import random 
 
 daytrip_destinations = ['Las Vegas', 'Paris', 'New York City', 'Miami', 'Topeka', 'San Diego']
 
@@ -7,30 +7,49 @@ daytrip_resteraunts = ['Hell"s Kitchen', 'la crossiants', 'mcdonalds', 'applebee
 daytrip_transportation = ['E-scooter', 'Uber', 'Taxi', 'Bus', 'Car', 'Limo', 'Heliocopter', 'plane','Walking']
 
 daytrip_entertainment = ['Laser tag', 'The gym, Hiking', 'Bingo palor', 'The casino', 'The bar or pub', 'Theater', 'Amusement Park']
+def run_day_trip_generator():
+    return
 
-import random 
-destinations = random.choice(daytrip_destinations)
-restaurant = random.choice(daytrip_resteraunts)
-transportation = random.choice(daytrip_transportation)
-entertainment = random.choice(daytrip_entertainment)
-
-
-
-destinations_2 = random.choice(daytrip_destinations)
-restaurant_2 = random.choice(daytrip_resteraunts)
-transportation_2 = random.choice(daytrip_transportation)
-entertainment_2 = random.choice(daytrip_entertainment)
+def Random_Generator():
+    return(results + results2 + results3+ results4)
+results = random.choice(daytrip_destinations)
+results2 = random.choice(daytrip_resteraunts)
+results3 = random.choice(daytrip_transportation)
+results4 = random.choice(daytrip_entertainment)
 
 
-daytrip = (f"{destinations} {restaurant} {transportation} {entertainment}")
 
-print(daytrip)
 
-endtrip = input("Did you like your daytrip y/n ")
 
-new_daytrip =(f"{destinations_2} {restaurant_2} {transportation_2} {entertainment_2}")
-while endtrip == "n":
-    print(new_daytrip)
-    break
-else:
-    print("Have a fun trip!")
+
+
+def Print_list():
+    return
+print(results)
+print(results2)
+print(results3)
+print(results4)
+def daytrip():
+    return
+user_satisfaction =input("did you enjoy this trip? y/n ")
+while user_satisfaction  != "n":
+ results = random.choice(daytrip_destinations)
+results2 = random.choice(daytrip_resteraunts)
+results3 = random.choice(daytrip_transportation)
+results4 = random.choice(daytrip_entertainment)
+print(results)
+print(results2)
+print(results3)
+print(results4)
+user_satisfaction = input("Were these options better? y/n ")
+
+  
+
+
+
+
+
+
+
+
+
